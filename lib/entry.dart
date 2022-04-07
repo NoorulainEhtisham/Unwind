1c4e03a08ff28adfb830bfded1165cacac7975f5
unknown <r.dodani_18551@khi.iba.edu.pk>
2022-04-07 12:16:27 +0500

    cognitive distortions

--- lib/entry.dart
+++ lib/entry.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:unwind_project/cognitive_distortions.dart';
 
 class Entry {
   static int key = 0;
@@ -7,16 +8,22 @@ class Entry {
   DateTime date;
   String note;
   bool isSelected = false;
+  CgnDistort cognitivedistortion = CgnDistort(type: '', example: '');
 
   Entry({title, required this.date, required this.note}) {
     this.title = title;
   }
 
-  void set setSelected(bool value) => isSelected = value;
+  void setCD(CgnDistort c) => this.cognitivedistortion = c;
+
+  void setSelected(bool value) => isSelected = value;
+
+  int get getkey=>key;
 
   void incKey()=>key++;
 
   void setTitle(String title) => this.title = title;
+
 }
 
 class Entries extends ChangeNotifier {
@@ -24,9 +31,24 @@ class Entries extends ChangeNotifier {
 
   List<Entry> get entries => _entries;
 
+  void setSelected(Entry entry, bool value) {
+    int index = _entries.indexOf(entry);
+    _entries[index].setSelected(value);
+    notifyListeners();
+  }
+
+  void setCognitiveDistortion(Entry entry, CgnDistort cd) {
+    int index = _entries.indexOf(entry);
+    _entries[index].setCD(cd);
+    notifyListeners();
+  }
+
   void EditEntry(Entry entry) {
-    if(_entries.contains(entry)) {
-      _entries.remove(entry);
+    for(var ent in _entries) {
+      if(ent.getkey==entry.getkey) {
+        _entries.remove(ent);
+        break;
+      }
     }
     AddEntry(entry);
     notifyListeners();
@@ -44,6 +66,7 @@ class Entries extends ChangeNotifier {
   void AddEntry(Entry entry) {
     _entries.add(entry);
     entry.incKey();
+    //print("Key = ${entry.getkey}");
     notifyListeners();
   }
 }
