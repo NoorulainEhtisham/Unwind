8c093ce0e8c608aad21ec0665ffef898b05d2ee2
areeshak <areeshakashif.k@gmail.com>
2022-04-10 01:34:59 +0500

    logo + favourites updates

--- lib/favourites.dart
+++ lib/favourites.dart
@@ -10,26 +10,11 @@ class FavouritesScreen extends StatefulWidget {
 
 class _FavouritesScreenState extends State<FavouritesScreen> {
   List<String> favItems = ["Rain 5", "Deep Breathing", "Simple"];
-  final List<Color> _colors = [
-    const Color(0xffF1D1FC),
-    const Color(0xffFCDBF8),
-    const Color(0xffE8FAFF),
-    const Color(0xffC9F1FF),
-    const Color(0xffC5D9FC),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: _colors,
-            )
-        ),
-        child: Center(
+      body:  Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -65,7 +50,6 @@ class _FavouritesScreenState extends State<FavouritesScreen> {
               )
             ],
           ),
-        ),
       ),
     );
   }
