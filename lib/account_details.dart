3b18d29d8865895e1b00b5a8fd3fe2c7b7f98b8a
NoorulainEhtisham <noorulain.ehtisham@gmail.com>
2022-05-18 03:40:21 +0500

    display account details done, but quotes issue remains

--- lib/account_details.dart
+++ lib/account_details.dart
@@ -22,72 +22,72 @@ class _AccountDetailsScreenState extends State<AccountDetailsScreen> {
       ),
       body: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15),
+            padding: const EdgeInsets.all(20),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 UserGoogleAccountInfo(),
                 //AccInfo(),
-                const Padding(padding: EdgeInsets.all(8)),
-                isEditable == false ?
-                IconButton(
-                    onPressed: () {
-                      isEditable = true;
-                      setState(() {
-
-                      });
-                    },
-                    icon: const Icon(Icons.edit)) : Container(),
-                const Padding(padding: EdgeInsets.all(50)),
-                const Text("Name", style: TextStyle(fontSize: 20),),
-                const Padding(padding: EdgeInsets.all(7)),
-                isEditable == false
-                    ? Container(
-                    width: MediaQuery.of(context).size.width*0.9,
-                    height : MediaQuery.of(context).size.height*0.06,
-                    padding: const EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        width: 0.3,
-                      ),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Text(_nameController.text))
-                    : TextField(
-                        controller: _nameController,
-                      ),
-                const Padding(padding: EdgeInsets.all(25)),
-                const Text("Email", style: TextStyle(fontSize: 20),),
-                const Padding(padding: EdgeInsets.all(7)),
-                isEditable == false
-                    ?  Container(
-                      width: MediaQuery.of(context).size.width*0.9,
-                      height : MediaQuery.of(context).size.height*0.06,
-                  padding: const EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                      border: Border.all(width: 0.3,),
-                      borderRadius: BorderRadius.circular(10),
-                  ),
-                      child: Text(_emailController.text))
-                    : TextField(
-                        controller: _emailController,
-                      ),
-                const Padding(padding: EdgeInsets.all(50)),
-                isEditable == true
-                    ? Center(
-                      child: SizedBox(
-                  width: MediaQuery.of(context).size.width*0.4,
-                  height : MediaQuery.of(context).size.height*0.06,
-                        child: ElevatedButton(
-                            onPressed: () {
-                              isEditable = false;
-                              setState(() {});
-                            },
-                            child: const Text("Save")),
-                      ),
-                    )
-                    : Container(),                                      //null
+                // const Padding(padding: EdgeInsets.all(8)),
+                // isEditable == false ?
+                // IconButton(
+                //     onPressed: () {
+                //       isEditable = true;
+                //       setState(() {
+                //
+                //       });
+                //     },
+                //     icon: const Icon(Icons.edit)) : Container(),
+                // const Padding(padding: EdgeInsets.all(50)),
+                // const Text("Name", style: TextStyle(fontSize: 20),),
+                // const Padding(padding: EdgeInsets.all(7)),
+                // isEditable == false
+                //     ? Container(
+                //     width: MediaQuery.of(context).size.width*0.9,
+                //     height : MediaQuery.of(context).size.height*0.06,
+                //     padding: const EdgeInsets.all(15),
+                //     decoration: BoxDecoration(
+                //       border: Border.all(
+                //         width: 0.3,
+                //       ),
+                //       borderRadius: BorderRadius.circular(10),
+                //     ),
+                //     child: Text(_nameController.text))
+                //     : TextField(
+                //         controller: _nameController,
+                //       ),
+                // const Padding(padding: EdgeInsets.all(25)),
+                // const Text("Email", style: TextStyle(fontSize: 20),),
+                // const Padding(padding: EdgeInsets.all(7)),
+                // isEditable == false
+                //     ?  Container(
+                //       width: MediaQuery.of(context).size.width*0.9,
+                //       height : MediaQuery.of(context).size.height*0.06,
+                //   padding: const EdgeInsets.all(15),
+                //   decoration: BoxDecoration(
+                //       border: Border.all(width: 0.3,),
+                //       borderRadius: BorderRadius.circular(10),
+                //   ),
+                //       child: Text(_emailController.text))
+                //     : TextField(
+                //         controller: _emailController,
+                //       ),
+                // const Padding(padding: EdgeInsets.all(50)),
+                // isEditable == true
+                //     ? Center(
+                //       child: SizedBox(
+                //   width: MediaQuery.of(context).size.width*0.4,
+                //   height : MediaQuery.of(context).size.height*0.06,
+                //         child: ElevatedButton(
+                //             onPressed: () {
+                //               isEditable = false;
+                //               setState(() {});
+                //             },
+                //             child: const Text("Save")),
+                //       ),
+                //     )
+                //     : Container(),                                      //null
               ],
             ),
           ),
